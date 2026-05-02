cf276b84d6fd72d51c945827b374c25276998d32
Sushmitha1508 <sush41354@gmail.com>
2026-05-02 11:24:16 +0530

    added codes

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,12 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 
-import vitest from '@vitest/eslint-plugin';   // if you're using Vitest
-// import jest from 'eslint-plugin-jest';     // uncomment if using Jest instead
-
-import { defineConfig, globalIgnores } from 'eslint/config';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-
-  // Main config for source files
+  globalIgnores(['dist','src/__tests__/**']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -21,30 +16,10 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      // Add any custom rules here
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+  
+])
 
-  // Test files config (Vitest)
-  {
-    files: ['**/*.{test,spec}.{js,jsx}'],
-    plugins: {
-      vitest,
-    },
-    rules: {
-      ...vitest.configs.recommended.rules,
-    },
-    languageOptions: {
-      globals: {
-        ...globals.jest, // or vitest globals if needed
-      },
-    },
-  },
-]);
+
